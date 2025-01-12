a6a6b9b1b9efe25542409eddaf043250a8b7a26d
Zenati Mehdi <mehdi1.zenati@epitech.eu>
2025-01-12 13:43:33 +0100

    Links to single product

--- testApp1/src/Controller/ProductController.php
+++ testApp1/src/Controller/ProductController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
-
+use App\Entity\Product;
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'app_product')]
@@ -14,9 +14,29 @@ class ProductController extends AbstractController
     {
         // $products = $tableProduct->findAll();
         // dump($products);
-        // dd($products) pour dump & die
+        // dd($products) for dump & die
         return $this->render('product/index.html.twig', [
             'products' => $tableProduct->findAll(),
         ]);
     }
+
+    // #[Route('/product/{id<\d+>}')]
+    // public function show($id, ProductRepository $tableProduct): Response {
+    //    $product = $tableProduct->findOneBy(['id' => $id]); // find($id) for ids
+    //     if ($product === null) {
+    //         throw $this->createNotFoundException('Product not found'); 
+    //     }
+    //     return $this->render('product/show.html.twig', [
+    //     'product' => $product,
+    //    ]);
+    // }
+
+    // same but faster
+    #[Route('/product/{id<\d+>}', name: 'app_product_show')]
+    public function show(Product $product): Response {
+        return $this->render('product/show.html.twig', [
+        'product' => $product,
+       ]);
+    }
+
 }
