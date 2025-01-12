fc99eecbb1b00ff8865282930ec827db5c380aa4
Zenati Mehdi <mehdi1.zenati@epitech.eu>
2025-01-12 14:17:13 +0100

    Form saved in DB

--- testApp1/src/Controller/ProductController.php
+++ testApp1/src/Controller/ProductController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
 use App\Entity\Product;
+use App\Form\ProductType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'app_product')]
@@ -37,6 +40,20 @@ class ProductController extends AbstractController
         return $this->render('product/show.html.twig', [
         'product' => $product,
        ]);
+    }
+    #[Route('/product/new', name: 'app_product_new')]
+    public function new(Request $request, EntityManagerInterface $manager): Response {
+        $product = new Product;
+        $form = $this->createForm(ProductType::class, $product);
+        $form -> handleRequest($request);
+        if($form->isSubmitted()){
+            $manager->persist($product);
+            $manager->flush();
+            return $this->redirectToRoute('app_product_show', ['id' => $product->getId()]);
+        }
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 
 }
